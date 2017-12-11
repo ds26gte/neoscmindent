e6e7d0e02fccedc683b6351440cbea70b934cae6
Dorai Sitaram <ds26gte@yahoo.com>
2017-12-11 14:38:01 -0500

    Add note on `if`

--- .lispwords.lua
+++ .lispwords.lua
@@ -1,12 +1,12 @@
 --[[
-Last modified: 2017-12-09
+.lispwords.lua
 Dorai Sitaram
+Last modified: 2017-12-12
 
-Config file used by neoscmindent.lua. Associates an indent number with a Lisp
-keyword. If keyword A's indent number is N, and a Lisp form introduced by A is
-split across lines, then if the Ith subform starts one such line, that line is
-indented by 3 columns past A if I <= N, and by 1 column past A otherwise. This
-file should be placed in $HOME.
+Associates an indent number with a Lisp keyword. If keyword A's indent number
+is N, and a Lisp form introduced by A is split across lines, then if the Ith
+subform starts one such line, that line is indented by 3 columns past A if I <=
+N, and by 1 column past A otherwise. This file should be placed in $HOME.
 --]]
 
 return {
@@ -15,6 +15,7 @@ return {
   ['do'] = 2,
   ['do*'] = 2,
   ['fluid-let'] = 1,
+  ['if'] = 2,
   ['lambda'] = 1,
   ['let'] = 1,
   ['let*'] = 1,
