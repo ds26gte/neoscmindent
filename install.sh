fd5ba7bf86895243c9f12393df673ef36046d88a
Dorai Sitaram <ds26gte@yahoo.com>
2019-11-13 15:20:07 -0500

    install.sh: create config dir if not already there, and generate helptags for doc.

--- install.sh
+++ install.sh
@@ -4,15 +4,22 @@
 VICONFIGDIR=${XDG_CONFIG_HOME:-~/.config}/nvim
 
 if test ! -d $VICONFIGDIR; then
-  echo You don\'t appear to be using Neovim\; trying Vim...
-  if test ! -d ~/.vim; then
-    echo You don\'t appear to be using Vim either.
-    echo Please see README.adoc on how to install neoscmindent
-    echo on your system.
-    exit
+  if test "$(which nvim)"; then
+    mkdir -p $VICONFIGDIR
   else
-    echo You appear to be using Vim.
+    echo You don\'t appear to be using Neovim\; trying Vim...
     export VICONFIGDIR=~/.vim
+    if test ! -d ~/.vim; then
+      if test "$(which vim)"; then
+        mkdir -p ~/.vim
+      else
+        echo You don\'t appear to be using Vim either.
+        echo Please see README.adoc on how to install neoscmindent
+        echo on your system.
+        exit
+      fi
+    fi
+    echo You appear to be using Vim.
   fi
 fi
 
@@ -27,4 +34,10 @@ if test -d ../neoscmindent; then
 else
   cd $PACKDIR
   git clone https://github.com/ds26gte/neoscmindent
+fi
+
+if test -d $PACKDIR/neoscmindent/doc; then
+  cd $PACKDIR/neoscmindent/doc
+  EXEDIT=$(if test "$(which nvim)"; then echo nvim; else echo vim;fi)
+  $EXEDIT -es -c 'helptags .'
 fi
